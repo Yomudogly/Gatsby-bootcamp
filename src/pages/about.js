d02b87fb8a4a3442024589c83807dd5e3eba2b5a
Yomudogly <khalnepesov@gmail.com>
2020-03-17 13:01:23 -0400

    added new pages

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I currently looking for a new job</p>
+        </div>
+    )
+}
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+    return (
+        <div>
+            <h1>Hi there,</h1>
+            <h2>I'm Roman, full-stack web developer based in Miamis</h2>
+        </div>
+    )
+}
+
+export default IndexPage
